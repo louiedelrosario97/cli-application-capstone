b10a87d2a0ba6197c2b0d7eb2f230d96f9ef64eb
Louie Delrosario <louie.delrosario97@gmail.com>
2026-04-27 11:26:51 -0700

    created homeScreen().

--- src/com/pluralsight/CPI_Application.java
+++ src/com/pluralsight/CPI_Application.java
@@ -1,4 +1,67 @@
 package com.pluralsight;
 
-public class CPI_Application {
+import java.util.Scanner;
+
+//
+public class CPI_Application
+{
+   static Scanner scanner = new Scanner(System.in);
+
+   static void homeScreen()
+   {
+       while(true) {
+           System.out.println();
+           System.out.println("ACCOUNTING LEDGER");
+           System.out.println("---------------------");
+           System.out.println();
+           System.out.println("D) Add Deposit");
+           System.out.println("P) Add Payment");
+           System.out.println("L) Ledger Display");
+           System.out.println("E) Exit");
+           System.out.print("Choose option: ");
+           String choice = scanner.nextLine().toUpperCase().strip();
+           System.out.println();
+
+           switch (choice) {
+               case "D":
+                   deposit();
+                   break;
+               case "P":
+                   payment();
+                   break;
+               case "L":
+                   ledgerScreen();
+                   break;
+               case "E":
+                   System.out.println("Goodbye!");
+                   break;
+               default:
+                   System.out.println("Invalid response, try again: ");
+                   break;
+           }
+       }
+
+       public
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
---
+++ src/com/pluralsight/Transaction.java
@@ -0,0 +1,35 @@
+package com.pluralsight;
+
+public class Transaction {
+
+    // Data Members
+    String date;
+    String time;
+    String description;
+    String vendor;
+    double amount;
+
+
+    // Getters
+    public double getAmount() {return amount;}
+    public String getVendor() { return vendor; }
+    public String getDescription() { return description; }
+    public String getTime() { return time; }
+    public String getDate() { return date; }
+
+    // Setters
+    public void setAmount(double amount) { this.amount = amount; }
+    public void setVendor(String vendor) { this.vendor = vendor; }
+    public void setDescription(String description) { this.description = description; }
+    public void setTime(String time) { this.time = time; }
+    public void setDate(String date) { this.date = date; }
+
+    // Constructor
+    public Transaction(String date, String time, String description, String vendor, double amount) {
+        this.date = date;
+        this.time = time;
+        this.description = description;
+        this.vendor = vendor;
+        this.amount = amount;
+    }
+}
